e0749bc458765ed8aa1773fc8d08ab93a78477f1
Khaanz.uwu <farkhanmorelix@gmail.com>
2022-06-19 14:00:22 +0700

    Initial commit

---
+++ src/pages/IndexHome.vue
@@ -0,0 +1,121 @@
+<template>
+<!-- <datepicker v-model="selected" :locale="locale" :upperLimit="to" :lowerLimit="from" clearable="true"></datepicker> -->
+  <q-page class="relative-position">
+    <q-scroll-area class="absolute fullscreen">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input bottom-slots v-model="newTweetContent" class="q-tweet" placeholder="What's your tweets now?" counter maxlength="250" autogrow>
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img src="https://i.pinimg.com/736x/d5/57/09/d55709eda0e977af153b3a4e381644a4.jpg">
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+        <div class="col col-shrink">
+          <q-btn @click="addNewTweet" :disable="!newTweetContent" color="primary" label="Tweet" class="q-mb-lg" unelevated rounded no-caps/>
+        </div>
+      </div>
+      <q-separator size="10px" color="grey-2" class="divider" />
+      
+      <q-list separator>
+        <transition-group
+          appear
+          enter-active-class="animated fadeIn"
+          leave-active-class="animated fadeOut"
+        >
+          <q-item v-for="tweet in tweets" :key="tweet.date" class="tweet q-py-md">
+            <q-item-section avatar top>
+              <q-avatar size="xl">
+                <img src="https://i.pinimg.com/736x/d5/57/09/d55709eda0e977af153b3a4e381644a4.jpg">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-subtitle1"><strong>Genshin Impact Official - ID</strong> <span class="text-grey-7">@GenshinImpactID &bull; {{tweet.date}}</span></q-item-label>
+              <q-item-label class="tweet-content text-body1">
+                {{tweet.content}}
+              </q-item-label>
+
+              <div class="tweet-icons row justify-between q-mt-sm">
+                <q-btn flat round color="grey" icon="far fa-comment" size="sm" />
+                <q-btn flat round color="grey" icon="fas fa-retweet" size="sm" />
+                <q-btn flat round color="grey" icon="far fa-heart" size="sm" />
+                <q-btn @click="deleteTweet(tweet)" flat round color="grey" icon="fas fa-trash" size="sm" />
+              </div>
+            </q-item-section>
+
+            <!-- <q-item-section side top>
+              {{tweet.date}}
+            </q-item-section> -->
+          </q-item>
+        </transition-group>
+
+        <q-separator inset="item" />
+      </q-list>
+    </q-scroll-area>
+  </q-page>
+</template>
+
+<script>
+// import { Datepicker } from '../src/datepicker/Datepicker.vue'
+import { formatDistance } from 'date-fns'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexHome',
+  data() {
+    return {
+      newTweetContent: '',
+      tweets: [
+        {
+          content: 'Genshin Impact Akan Segera Rilis di GeForce NOW ! Dear Traveler, Uji coba Genshin Impact di GeForce NOW akan segera selesai dan akan resmi dirilis sepenuhnya pada minggu depan. Traveler dapat memainkan Genshin Impact dari jarak jauh tanpa perlu mengunduh data game!',
+          // date: new Date().getUTCSeconds()
+          date: '2 Minutes'
+        },
+        {
+          content: '[Event Berhadiah] Kontes Fan Art "Hanamizaka Heroics" Arataki Itto Dimulai! Dear Traveler, Kontes Fan Art Arataki Itto sudah dimulai! Siapkan alat gambarmu dan ikuti eventnya~ [Waktu Event] 15 Juni 2022 - 6 Juli 2022 23:59 (UTC+8) Lihat pengumuman di sini > > > https://genshin.hoyoverse.com/id/news/detail/21490 #GenshinImpact #HoYoverse #AratakiItto',
+          date: '1 day ago'
+        }
+      ]
+    }
+  },
+  methods: {
+    addNewTweet() {
+      let newTweet = {
+        content: this.newTweetContent,
+        date: 'a few minutes ago'
+      }
+      this.tweets.unshift(newTweet)
+      this.newTweetContent = ''
+    },
+    deleteTweet(tweet) {
+      let dateToDelete = tweet.date
+      let index = this.tweets.findIndex(tweet => tweet.date === dateToDelete)
+      this.tweets.splice(index,1)
+    }
+  },
+  filters: {
+    relativeDate(value) {
+      return formatDistance(value, new Date())
+    }
+  },
+})
+</script>
+
+<style lang="sass">
+.q-tweet
+  textarea
+    font-size: 19px
+    line-height: 1.4 !important
+.divider
+ border-top: 1px solid
+ border-bottom: 1px solid
+ border-color: $grey-4
+ .tweet-content
+   white-space: pre-line 
+.tweet-icons
+  margin-left: 5px
+.tweet:not(:first-child)
+  border-top: 1px solid rgba(0, 0, 0, 0.12)
+</style>
